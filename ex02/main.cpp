7c1e2dce02a86799fc5442db423a6f7d9cdfb95b
cecompte <cecile.compte@gmail.com>
2025-12-23 13:13:05 +0100

    ex02 additional tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cecompte <cecompte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 09:16:42 by cecompte          #+#    #+#             */
-/*   Updated: 2025/12/23 12:50:18 by cecompte         ###   ########.fr       */
+/*   Updated: 2025/12/23 13:12:28 by cecompte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int main( void )
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
+	std::cout << "Tests required in the subject: \n";
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
@@ -25,5 +26,17 @@ int main( void )
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
 
+	std::cout << "\nAdditional tests: \n";
+	Fixed c(42);
+	Fixed d(-12);
+	std::cout << "c = " << c << std::endl;
+	std::cout << "d = " << d << std::endl;
+	std::cout << "c + d = " << c + d << std::endl;
+	std::cout << "c - d = " << c - d << std::endl;
+	std::cout << "c / d = " << c / d << std::endl;
+	std::cout << "min(c, d) = " << Fixed::min(c, d) << std::endl;
+	std::cout << "--d = " << --d << std::endl;
+	std::cout << "d-- = " << d-- << std::endl;
+	std::cout << "d = " << d << std::endl;
 	return (0);
 }
